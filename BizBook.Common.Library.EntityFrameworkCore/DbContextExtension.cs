7f30f43c8d61b960593e0469efbf97c13eea88ed
Foyzul Karim <foyzulkarim@gmail.com>
2020-11-01 12:57:21 +0800

    Added stripped down version of BizBook.Common.Library

---
+++ BizBook.Common.Library.EntityFrameworkCore/DbContextExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BizBook.Common.Library.EntityFrameworkCore
+{
+
+    public class IsIndex : Attribute
+    {
+
+    }
+
+    public static class DbContextExtension
+    {
+        public static ModelBuilder BuildIndex<T>(this ModelBuilder builder) where T : class
+        {
+            var propertyInfos = typeof(T).GetProperties().ToList().Where(x => x.GetCustomAttributes(typeof(IsIndex), false).Length > 0);
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var parameterExpression = Expression.Parameter(typeof(T), "x");
+                var memberExpression = Expression.Convert(Expression.Property(parameterExpression, propertyInfo.Name), typeof(object));
+                var lambda = Expression.Lambda<Func<T, object>>(memberExpression, parameterExpression);
+                string indexName = $"IX_{propertyInfo.Name}";
+                builder.Entity<T>().HasIndex(lambda).HasName(indexName);
+            }
+
+            return builder;
+        }
+    }
+}
